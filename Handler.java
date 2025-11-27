5de8e790f9d17daf2a6b0d7b6b7bbd05139728f5
Jack <jackb77932@gmail.com>
2025-11-27 00:02:17 -0700

    Currently incomplete, additions to Connection, Handler, Server. Additional file ReaderThread. It is my intention to ask Garfield about the use of additional ports to accomodate text streams vs image streams. ReaderThread will eventually work with MyGUI, but is not currently implemented, mostly on account of MyGUI not being fully implemented.

--- Handler.java
+++ Handler.java
@@ -8,8 +8,18 @@
 
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.*;
 
 public class Handler {
+    //thread manager
+    private static final Executor execClient = Executors.newVirtualThreadPerTaskExecutor();
 
-    
+    //process
+    public void process(Socket client) throws java.io.IOException {
+        ChatScreen chatscreen = null;
+
+        Runnable task = new ReaderThread(client, chatscreen);
+        execClient.execute(task);
+
+    }
 }
