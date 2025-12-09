50df2e53da0cddf461627de52f4406d93d489838
heidi-maybe <skihorse228@gmail.com>
2025-12-09 14:51:21 -0700

    Added Commentst

--- Handler.java
+++ Handler.java
@@ -30,7 +30,7 @@ public class Handler {
                 }
 
                 // A message was sent in the chatroom 
-                if (message.key.equals("MSG")) { // \0 is NULL padding 
+                if (message.key.equals("MSG")) { // \0 is NULL padding and striped in the encorder so doesn't matter here (cries)
                     String valueStr = new String(message.value, StandardCharsets.UTF_8);
                     System.out.println("Received: " + message.key + ":" + valueStr);
                 }
